f49001a7321da564fb4cdc7790214fe178d5e449
Maggie Walker <mags21walker@gmail.com>
2019-06-20 12:49:58 -0400

    Deleted unused JS file, serving static html

--- index.js
+++
@@ -1,6 +0,0 @@
-
-const button = document.getElementById('button');
-
-button.addEventListener('click', function() {
-    console.log('heeeeelooooo')
-})
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
+const path = require('path');
 const http = require('http');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 const app = express();
 
-app.post('/sms', (req, res) => {
-  const twiml = new MessagingResponse();
-  twiml.message('Thanks for submitting your message to Goddess Thoughts!!!');
-
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
-});
+app.use(express.static(path.join(__dirname, 'public')))
 
 http.createServer(app).listen(1337, () => {
   console.log('Express server listening on port 1337');
 });
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+  twiml.message('Thanks for submitting your message to Goddess Thoughts!!!');
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
