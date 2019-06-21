2827fd44e9a5541407963083da20d77d8e823608
Maggie Walker <mags21walker@gmail.com>
2019-06-21 14:45:29 -0400

    testing

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ http.createServer(app).listen(PORT || 1337, () => {
 
 app.post('/sms', (req, res) => {
   let cache = [];
-  console.log('I received a message', JSON.stringify(req, function(key, value) {
+  console.log('I received a message', JSON.stringify(req.params, function(key, value) {
         if (typeof value === 'object' && value !== null) {
             if (cache.indexOf(value) !== -1) {
                 // Duplicate reference found, discard key
